991b4f0255d2cb808c832c18624f7ef59fa622ec
rui-exe <mrswager173@gmail.com>
2022-10-10 17:03:42 +0100

    internal representation complete

--- trabalho.hs
+++ trabalho.hs
@@ -1,3 +1,4 @@
+import Data.Char ( isDigit, isAlpha )
 polynomialCleaner :: String -> String
 polynomialCleaner [] = []
 polynomialCleaner (x:xs)
@@ -13,3 +14,15 @@ polynomialSimplifier (x:xs)
 
 polynomialOrganizer :: String -> [String]
 polynomialOrganizer xs = polynomialSimplifier (polynomialCleaner xs)
+
+internalRepresentation :: [String] -> [(Int,Char ,Int)]
+internalRepresentation xs
+    |null xs = []
+    |all isDigit (head xs) = (read (head xs), ' ', 0) : internalRepresentation (tail xs)
+    |head (head xs) == '-' = (-read (takeWhile isDigit (pos_mon)) :: Int, head (dropWhile isDigit (pos_mon)), exponent) : internalRepresentation (drop 1 xs)
+    |otherwise = (read (takeWhile isDigit (head xs)) :: Int, head (dropWhile isDigit (head xs)), exponent) : internalRepresentation (drop 1 xs)
+    where pos_mon = tail (head xs)
+          exponent  = read (tail (dropWhile (/= '^') (head xs))) :: Int 
+
+iR :: String -> [(Int,Char ,Int)]
+iR xs = internalRepresentation (polynomialOrganizer xs)
