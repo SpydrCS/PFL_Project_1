21e79476bb9df1e61107d41f4296ba2c099212fe
Guilherme Moreira <gui29102002@gmail.com>
2022-10-13 22:40:07 +0000

    Finished a,b,d

--- trabalho.hs
+++ trabalho.hs
@@ -43,10 +43,22 @@ moreSimple :: [((Char,Int),Int)] -> [(Int, Char, Int)] -- changes representation
 moreSimple xs = [(a,b,c) | ((b,c),a) <- xs]
 
 tplToString :: [(Int, Char, Int)] -> [String] -- joins tuple into understandable list of strings
-tplToString xs = [(show a) ++ "*" ++ [b] ++ "^" ++ (show c) | (a,b,c) <- xs, a > 1, c > 1] ++ [(show a) ++ "*" ++ [b] | (a,b,c) <- xs, a > 1, c == 1] ++ [[b] | (a,b,c) <- xs, a == 1, c == 1] ++ [[b] ++ "^" ++ (show c) | (a,b,c) <- xs, a == 1, c > 1]
+tplToString xs = [(show a) ++ "*" ++ [b] ++ "^" ++ (show c) | (a,b,c) <- xs, a > 1, c > 1] ++ [(show a) ++ "*" ++ [b] | (a,b,c) <- xs, a > 1, c == 1] ++ [[b] | (a,b,c) <- xs, a == 1, c == 1] ++ [[b] ++ "^" ++ (show c) | (a,b,c) <- xs, a == 1, c > 1] ++ [show a | (a,b,c) <- xs, b == ' ']
 
 joiner :: [String] -> String -- joins list of strings into one string
 joiner xs = foldr (\a b-> a ++ if b=="" then b else if (head b)=='-' then " - " ++ (drop 1 b) else " + " ++ b) "" xs
 
-iR :: String -> String -- main function to run
-iR xs = joiner (tplToString (moreSimple (simply (sorting (internalRepresentation (polynomialOrganizer xs))))))
+derive :: [(Int, Char, Int)] -> [(Int, Char, Int)]
+derive xs = [(a*c,b,c-1) | (a,b,c) <- xs, c > 1] ++ [(a,' ',0) | (a,b,c) <- xs, c == 1]
+
+normalize :: String -> String -- main function to run option a
+normalize poly = joiner (tplToString (moreSimple (simply (sorting (internalRepresentation (polynomialOrganizer poly))))))
+
+add :: String -> String -> String -- main function to run option b
+add poly1 poly2 = joiner (tplToString (moreSimple (simply (sorting (internalRepresentation (polynomialOrganizer poly1 ++ polynomialOrganizer poly2))))))
+
+-- multiply :: String -> String -> String
+-- multiply poly1 poly2 =
+
+derivative :: String -> String -- main function to run option d
+derivative poly = joiner (tplToString (moreSimple (simply (sorting (derive (moreSimple (simply (sorting (internalRepresentation (polynomialOrganizer poly))))))))))
