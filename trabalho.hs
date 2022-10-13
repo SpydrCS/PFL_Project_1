127357c27d2f95d360a5d16d903e6659036fe417
Guilherme Moreira <gui29102002@gmail.com>
2022-10-13 15:42:54 +0000

    Finished internal representation

--- trabalho.hs
+++ trabalho.hs
@@ -15,14 +15,20 @@ polynomialSimplifier (x:xs)
 polynomialOrganizer :: String -> [String]
 polynomialOrganizer xs = polynomialSimplifier (polynomialCleaner xs)
 
+maybeTail :: [Char] -> String
+maybeTail [] = "1"
+maybeTail xs = tail xs
+
 internalRepresentation :: [String] -> [(Int,Char ,Int)]
 internalRepresentation xs
     |null xs = []
     |all isDigit (head xs) = (read (head xs), ' ', 0) : internalRepresentation (tail xs)
+    |isAlpha (head (head xs)) = (1, head (dropWhile isDigit (head xs)), exponent) : internalRepresentation (drop 1 xs)
+    |isAlpha ((head xs)!!1) && head (head xs) == '-' = [(-1, (head xs)!!1, exponent)]
     |head (head xs) == '-' = (-read (takeWhile isDigit (pos_mon)) :: Int, head (dropWhile isDigit (pos_mon)), exponent) : internalRepresentation (drop 1 xs)
     |otherwise = (read (takeWhile isDigit (head xs)) :: Int, head (dropWhile isDigit (head xs)), exponent) : internalRepresentation (drop 1 xs)
     where pos_mon = tail (head xs)
-          exponent  = read (tail (dropWhile (/= '^') (head xs))) :: Int 
+          exponent  = read (maybeTail (dropWhile (/= '^') (head xs))) :: Int
 
 iR :: String -> [(Int,Char ,Int)]
 iR xs = internalRepresentation (polynomialOrganizer xs)
