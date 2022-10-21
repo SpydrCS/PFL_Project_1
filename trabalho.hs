5e0a1128c0dab2708b9c826edf30872161383405
Guilherme Moreira <gui29102002@gmail.com>
2022-10-21 12:13:33 +0000

    Commented functions

--- trabalho.hs
+++ trabalho.hs
@@ -1,23 +1,23 @@
 import Data.Char ( isDigit, isAlpha, isLetter )
 import qualified Data.Map as M
 
-polynomialCleaner :: String -> String
+polynomialCleaner :: String -> String -- removes spaces and '*' from strings e.g "2*x*y + 3" = "2xy+3"
 polynomialCleaner [] = []
 polynomialCleaner (x:xs)
     | x == ' ' = polynomialCleaner xs
     |x == '*' = polynomialCleaner xs
     | otherwise = x : polynomialCleaner xs
 
-polynomialSimplifier :: String -> [String]
+polynomialSimplifier :: String -> [String] -- separates string into list of strings, by '+' or '-' e.g "2xy+3" = ["2xy","3"]
 polynomialSimplifier [] = []
 polynomialSimplifier (x:xs)
     |x == '+' = polynomialSimplifier xs
     |otherwise = (x : takeWhile (\x -> (x /='+') && (x /='-')) xs) : polynomialSimplifier (dropWhile (\x -> (x /='+') && (x /='-')) xs)
 
-polynomialOrganizer :: String -> [String]
+polynomialOrganizer :: String -> [String] -- converts string to list of strings divided by '+' or '-' e.g "2*x*y + 3" = ["2xy","3"]
 polynomialOrganizer xs = polynomialSimplifier (polynomialCleaner xs)
 
-maybeTail :: [Char] -> String -- in case list is empty, return "1", otherwise return tail of list
+maybeTail :: [Char] -> String -- in case list is empty, return "1", otherwise return tail of list e.g ['1', '2', '3'] = "3"
 maybeTail [] = "1"
 maybeTail xs = tail xs
 
@@ -52,10 +52,10 @@ polynomialSorter :: [String] -> [String]
 polynomialSorter [] = []
 polynomialSorter (x:xs) = polynomialSorter [y | y <- xs, polynomialGreaterThan y x] ++ [x] ++ polynomialSorter [y | y <- xs, not (polynomialGreaterThan y x)]
 
-sorting :: [(Int,[(Char,Int)])] -> [([(Char,Int)],[Int])]  -- takes the list with all variables (some repeated) and groups them without repetition
-sorting assocs = M.toList (M.fromListWith (\n1 n2 -> [sum(n1 ++ n2)]) [((b), [a]) | (a,b) <- assocs]) -- [(1,('y',1)),(2,('y',1))] becomes [(('y',1),[3])], etc
+sorting :: [(Int,[(Char,Int)])] -> [([(Char,Int)],[Int])]  -- takes the list with all variables (some repeated) and groups them without repetition e.g [(1,('y',1)),(2,('y',1))] = [(('y',1),[3])]
+sorting assocs = M.toList (M.fromListWith (\n1 n2 -> [sum(n1 ++ n2)]) [((b), [a]) | (a,b) <- assocs])
 
-simply :: [([(Char,Int)],[Int])] -> [([(Char,Int)],Int)] -- changes count number from list to normal int
+simply :: [([(Char,Int)],[Int])] -> [([(Char,Int)],Int)] -- converts second item in tuple form list of ints to normal int e.g [([(y,1)],[1])] = [([(y,1)],1)]
 simply xs = [(a,b) | ((a,[b])) <- xs]
 
 simplifyVariables :: [(Char,Int)] -> [String] -- simplifies internal representation of variables e.g [('x',2),('y',1)] = ["x^2","y"]
@@ -67,11 +67,37 @@ joinVariables xs = foldr (\a b -> a ++ if b == "" then b else "*" ++ b) "" xs
 simpVar :: [(Char,Int)] -> String -- from [('x',2),('y',1)] to "x^2*y"
 simpVar xs = joinVariables (simplifyVariables xs)
 
-tplToString :: [([(Char,Int)],Int)] -> [String] -- [([(y,1)],1)]
+tplToString :: [([(Char,Int)],Int)] -> [String] -- converts list of monomials from internal representation to understandable string e.g [([('y',1)],1),([('x',1)],2)] = ["2*x","y"]
 tplToString xs = [(show b) ++ "*" ++ (simpVar a) | (a,b) <- xs, b /= 1, a /= []] ++ [simpVar a | (a,b) <- xs, b == 1, a /= []] ++ [(show b) | (a,b) <- xs, a == []]
 
-joiner :: [String] -> String -- joins list of strings into one string
+joiner :: [String] -> String -- joins list of strings into one string e.g ["2*x","y"] = "2*x + y"
 joiner xs = foldr (\a b-> a ++ if b=="" then b else if (head b)=='-' then " - " ++ (drop 1 b) else " + " ++ b) "" xs
+
+simplifyexponents :: [(Char,Int)] -> [(Char,Int)] -- simplifies exponents of variables e.g [('x',2),('x',1)] = [('x',3)]
+simplifyexponents xs = [(a, sum b) | (a,b) <- M.toList (M.fromListWith (\n1 n2 -> n1 ++ n2) [(a,[b]) | (a,b) <- xs])]
+
+multiplyVars :: [(Char,Int)] -> [(Char,Int)] -> [(Char,Int)] -- multiplies variables of 2 monomials e.g [('x',2)] [('x',1)] = [('x',2),('x',1)]
+multiplyVars x y = x ++ y
+
+multiplyOne :: (Int,[(Char,Int)]) -> (Int,[(Char,Int)]) -> (Int,[(Char,Int)]) -- multiplies two monomials by themselves
+multiplyOne (a,b) (c,d) = (a*c, simplifyexponents(multiplyVars b d))
+
+multiply :: [(Int,[(Char,Int)])] -> [(Int,[(Char,Int)])] -> [(Int,[(Char,Int)])] -- multiplies two polynomials by one another
+multiply [] _ = []
+multiply (x:xs) ys = [multiplyOne x y | y<- ys] ++ multiply xs ys
+
+reducer :: [(Char,Int)] -> Char -> [(Char,Int)] -- reduces exponent of variable to be derived e.g [('y',1),('x',2)] 'x' = [('y',1),('x',1)]
+reducer xs vari = [(a,b) | (a,b) <- xs, a /= vari] ++ [(a,b-1) | (a,b) <- xs, a==vari, b>1]
+
+maybeHead :: [Int] -> Int -- in case list is empty, return 0, otherwise return head of list e.g [1, 2, 3] = 1
+maybeHead [] = 0
+maybeHead xs = head xs
+
+exponentt :: [(Char,Int)] -> Char -> Int -- finds exponent of variable to be derived e.g [('y',1),('x',2)] 'x' = 2
+exponentt xs vari = maybeHead [b | (a,b) <- xs, a==vari]
+
+changer :: [(Int,[(Char,Int)])] -> Char -> [(Int,[(Char,Int)])] -- changes internal tuples to be derived by the variable chosen e.g [(1,[('y',1),('x',1)]),(2,[('x',1),('y',2)])] 'y' = [(1,[('x',1)]),(4,[('x',1),('y',1)])]
+changer xs vari = [(a*(exponentt b vari),reducer b vari) | (a,b) <- xs, a*(exponentt b vari)/=0]
 
 normalize :: String -> String -- main function to run option a (normalize polynomial)
 normalize poly = joiner (polynomialSorter (tplToString (simply (sorting [internalRepresentation x | x <- polynomialOrganizer poly, head x /= '0']))))
@@ -79,42 +105,14 @@ normalize poly = joiner (polynomialSorter (tplToString (simply (sorting [interna
 add :: String -> String -> String -- main function to run option b (add 2 polynomials)
 add poly1 poly2 = normalize (poly1 ++ "+" ++ poly2)
 
-simplifyexponents :: [(Char,Int)] -> [(Char,Int)] -- simplifies exponents of variables e.g [('x',2),('x',1)] = [('x',3)]
-simplifyexponents xs = [(a, sum b) | (a,b) <- M.toList (M.fromListWith (\n1 n2 -> n1 ++ n2) [(a,[b]) | (a,b) <- xs])]
-
-
-multiplyVars :: [(Char,Int)] -> [(Char,Int)] -> [(Char,Int)] -- multiplies variables of a 2 monomials
-multiplyVars x y = x ++ y
-
-multiplyOne :: (Int,[(Char,Int)]) -> (Int,[(Char,Int)]) -> (Int,[(Char,Int)]) -- multiplies two monomials by themselves
-multiplyOne (a,b) (c,d) = (a*c, simplifyexponents(multiplyVars b d))
-
-multiply :: [(Int,[(Char,Int)])] -> [(Int,[(Char,Int)])] -> [(Int,[(Char,Int)])] -- multiplies 2 polynomials by one another
-multiply [] _ = []
-multiply (x:xs) ys = [multiplyOne x y | y<- ys] ++ multiply xs ys
-
-
-multiplication :: String -> String -> String -- main function to run option c
+multiplication :: String -> String -> String -- main function to run option c (multiply 2 polynomials)
 multiplication poly1 poly2 = joiner(polynomialSorter (tplToString (simply (sorting (multiply [internalRepresentation x | x <- polynomialOrganizer poly1, head x /= '0'] [internalRepresentation x | x <- polynomialOrganizer poly2, head x /= '0'])))))
 
-reducer :: [(Char,Int)] -> Char -> [(Char,Int)] -- reduces exponent of variable to be derived e.g [('y',1),('x',2)] 'x' = [('y',1),('x',1)]
-reducer xs vari = [(a,b) | (a,b) <- xs, a /= vari] ++ [(a,b-1) | (a,b) <- xs, a==vari, b>1]
-
-maybeHead :: [Int] -> Int -- in case list is empty, return "1", otherwise return head of list
-maybeHead [] = 0
-maybeHead xs = head xs
-
-exponentt :: [(Char,Int)] -> Char -> Int -- finds exponent of variable to be derived e.g [('y',1),('x',2)] 'x' = 2
-exponentt xs vari = maybeHead [b | (a,b) <- xs, a==vari]
-
-changer :: [(Int,[(Char,Int)])] -> Char -> [(Int,[(Char,Int)])] -- changes internal tuples to be derived by the variable chosen
-changer xs vari = [(a*(exponentt b vari),reducer b vari) | (a,b) <- xs, a*(exponentt b vari)/=0] -- e.g [(1,[('y',1),('x',1)]),(2,[('x',1),('y',2)])] 'y' = [(1,[('x',1)]),(4,[('x',1),('y',1)])]
-
-derivative :: String -> Char -> String -- main function to run option d
+derivative :: String -> Char -> String -- main function to run option d (derive polynomial)
 derivative poly vari | joiner (polynomialSorter (tplToString (simply (sorting (changer [internalRepresentation x | x <- polynomialOrganizer poly, head x /= '0'] vari))))) /= "" = joiner (polynomialSorter (tplToString (simply (sorting (changer [internalRepresentation x | x <- polynomialOrganizer poly] vari)))))
                      | otherwise = "0"
 
-main :: IO()
+main :: IO() -- main menu to choose what option you want to run
 main = do
             putStrLn "What do you want to do? Normalize (1), Add (2), Multiply (3), or Differentiate (4)?"
             str <- getLine
